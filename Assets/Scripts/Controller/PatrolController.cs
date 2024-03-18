036c9e0c02d14aae04d50469ce03b612154b02ab
johnloser <john-masterchief@hotmail.com>
2024-03-18 15:50:49 -0700

    add patrol enemy

---
+++ Assets/Scripts/Controller/PatrolController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Health;
+using UnityEngine;
+
+namespace Controller
+{
+    public class PatrolController : BaseController
+    {
+        [SerializeField] private bool _enableGizmos = true;
+        
+        [SerializeField] private float _patrolDistance = 10.0f;
+
+        [SerializeField] private float _pushigForce = 2.0f;
+
+
+        private Vector2 _leftEdge;
+        private Vector2 _rightEdge;
+        private bool _movingRight = true;
+        
+        protected override void Start()
+        {
+            base.Start();
+            var position = transform.position;
+            
+            _leftEdge = new Vector2(position.x - _patrolDistance / 2.0f, position.y);
+            _rightEdge = new Vector2(position.x + _patrolDistance / 2.0f, position.y);
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            InputHandler();
+        }
+
+        private void InputHandler()
+        {
+            var position = transform.position;
+            if (!IsFlipped && position.x > _rightEdge.x || IsFlipped && position.x < _leftEdge.x)
+            {
+                _movingRight = !_movingRight;
+            }
+            
+            _horizontalAxis = _movingRight ? -1 : 1;
+            
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (!other.gameObject.CompareTag("Player")) return;
+
+            other.gameObject.TryGetComponent<CharacterHealth>(out CharacterHealth playerHealth);
+            if (!playerHealth) return;
+            playerHealth.TakeDamage(this);
+            var rg = other.rigidbody;
+            var dir = other.transform.position.x - transform.position.x;
+            rg.AddForce(new Vector2(dir > 0 ? 1:-1, 1.0f) * _pushigForce, ForceMode2D.Impulse);
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!_enableGizmos) return;
+            var position = transform.position;
+            var leftEdge = new Vector2(position.x - _patrolDistance / 2.0f, position.y);
+            var rightEdge = new Vector2(position.x + _patrolDistance / 2.0f, position.y);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(leftEdge, 0.5f);
+            Gizmos.DrawWireSphere(rightEdge, 0.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/Effect/FlashEffect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Effect
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class FlashEffect : MonoBehaviour
+    {
+        [SerializeField] private float _flashTime = 0.2f;
+        
+        private SpriteRenderer _spriteRenderer;
+        private WaitForSeconds _waitForSeconds;
+
+        private void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _waitForSeconds = new WaitForSeconds(_flashTime);
+        }
+
+        public void Flash()
+        {
+            StartCoroutine(FlashEnumerator());
+        }
+
+        private IEnumerator FlashEnumerator()
+        {
+            _spriteRenderer.color = Color.red;
+            yield return _waitForSeconds;
+            _spriteRenderer.color = Color.white;
+        }
+    }
+}
+
