47dc1c266810650e77704d040ed65c9aa6e1224f
johnloser <66107110+johnloser-lwi@users.noreply.github.com>
2024-03-17 18:27:28 -0700

    move shared controller functions to BaseController.cs, add comments

---
+++ Assets/Scripts/Controller/BaseController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using UnityEngine;
+
+namespace Controller
+{
+    // Base class for all controllers, shares common functionalities for movement and ground check.
+    // This class is meant to be inherited by other controllers, such as PlayerController and EnemyController,
+    // cannot be used on its own
+    public abstract class BaseController : MonoBehaviour
+    {
+        // Properties
+        // Getter and Setter can be used to add additional logic when the value is changed
+        public bool IsGrounded
+        {
+            get => _isGrounded;
+            private set
+            {
+                if (value != _isGrounded)
+                {
+                    _isGrounded = value;
+                    _isGroundedChanged = true;
+                }
+            }
+        }
+    
+        public bool IsWalking 
+        {
+            get => _isWalking;
+            private set
+            {
+                if (value != _isWalking)
+                {
+                    _isWalking = value;
+                    _isWalkingChanged = true;
+                }
+            }
+        }
+
+        // This property is to make sure we don't actually flip the sprite if it's already flipped
+        public bool IsFlipped
+        {
+            get => _spriteRenderer.flipX;
+            private set
+            {
+                if (value != _spriteRenderer.flipX) _spriteRenderer.flipX = value;
+            }
+        }
+    
+    
+        // Serialized fields
+        [Header("Movement Settings")]
+        [SerializeField][Tooltip("Max speed the player can walk.")] 
+        protected float _moveSpeed = 50.0f;
+        [SerializeField][Tooltip("How fast the player accelerates to max speed when walking.")] 
+        protected float _acceleration = 2.0f;
+        [SerializeField][Tooltip("How fast the player decelerates to 0 when not walking.")] 
+        protected float _friction = 2.0f;
+        
+        [Space(10)]
+        [Header("Ground Check")]
+        [SerializeField][Tooltip("If true, the controller will check if it's grounded.")] 
+        protected bool _needGroundCheck = true;
+        [SerializeField][Tooltip("Layer mask to check if the controller is grounded. (Can be ignored if needGroundCheck is false)")] 
+        protected LayerMask _groundLayer;
+        [SerializeField][Tooltip("Tag to check if the controller is grounded. (Can be ignored if needGroundCheck is false)")] 
+        protected string _groundTag = "Ground"; // This one might be redundant since I've already defined the groundLayer,
+                                                // but it's here for the sake of the example
+    
+        // Components
+        protected SpriteRenderer _spriteRenderer;
+        protected CapsuleCollider2D _collider;
+        protected Rigidbody2D _rigidbody;
+        protected Animator _animator;
+    
+        // protected fields
+        protected float _horizontalAxis;
+        protected float _lastPositionY;
+        protected bool _isWalking;
+        protected bool _isWalkingChanged;
+        protected bool _isGrounded;
+        protected bool _isGroundedChanged;
+
+        protected virtual void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _collider = GetComponent<CapsuleCollider2D>();
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+        }
+
+        protected virtual void Update()
+        {
+            FlipSprite();
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            // Anything related to physics should be done in FixedUpdate
+            Movement();
+            GroundCheck();
+        }
+    
+        protected virtual void Movement()
+        {
+            // Horizontal movements
+            var moveInputForce = new Vector2(_horizontalAxis * _acceleration * 1000.0f * Time.deltaTime, 0);
+        
+            // Check if the character is walking base on the input
+            if (IsGrounded) IsWalking = moveInputForce.x != 0;
+            else IsWalking = false;
+        
+            // Apply force to the rigidbody
+            _rigidbody.AddForce(moveInputForce);
+
+            // Limit the speed of the character to the max speed
+            var currentVel = _rigidbody.velocity;
+            var speedAbs = Mathf.Abs(currentVel.x);
+            if (speedAbs > _moveSpeed)
+            {
+                _rigidbody.velocity = new Vector2(currentVel.x / speedAbs * _moveSpeed, currentVel.y);
+            }
+            
+            // Add friction when movement input stops, skip if there's an active movement input
+            if (moveInputForce.x != 0) return;
+            
+            currentVel = _rigidbody.velocity;
+            speedAbs = Mathf.Abs(currentVel.x);
+            
+            // When calculating float values with vectors, it's more efficient to calculate the float value first
+            // and then multiply it with the vector, instead of multiplying the vector with multiple float values
+            var frictionAmount = Vector2.right * (_friction * 1000.0f * Time.deltaTime);
+            var canBeStoppedByFriction = speedAbs - frictionAmount.x < 0 && IsGrounded;
+            var isTooSlow = speedAbs < 0.1f;
+            
+            // Stop the player if the speed is too low or if the friction can stop the player
+            if (isTooSlow || canBeStoppedByFriction)
+            {
+                _rigidbody.velocity = Vector2.up * currentVel.y; 
+                return;
+            }
+        
+            // Apply friction when grounded
+            if (!IsGrounded) return;
+            if (currentVel.x < 0) frictionAmount *= -1;
+            _rigidbody.AddForce(-frictionAmount);
+        }
+
+        protected virtual void GroundCheck()
+        {
+            // If the controller doesn't need to check if it's grounded, then it's always grounded
+            // Skipping the raycast will save some performance
+            if (!_needGroundCheck)
+            {
+                IsGrounded = true;
+                return;
+            }
+
+            // caching the position of the controller can save some performance too
+            var position = transform.position;
+        
+            // Only check grounded if the player has moved in the Y axis
+            // This will save some performance by not checking every frame since there's no need to
+            // check if the player is grounded if it hasn't moved in the Y axis
+            if (Math.Abs(position.y - _lastPositionY) < 0.01f) return;
+            _lastPositionY = position.y;
+        
+            // Raycast origin should be at the bottom of the collider
+            // The collider's pivot is at the center, so we need to subtract half of the collider's height
+            var origin = new Vector2(position.x, position.y - _collider.size.y / 2.0f);
+            
+            // By defining the layer mask in the raycast, we can save some performance by not checking every collider
+            RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, 0.1f, _groundLayer);
+            IsGrounded = hit && hit.transform.CompareTag(_groundTag);
+        }
+
+        protected virtual void FlipSprite()
+        {
+            if (_horizontalAxis == 0) return;
+            IsFlipped = _horizontalAxis < 0;
+        }
+    }
+}
--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -1,199 +1,103 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Controller
 {
-    // Properties
-    public bool IsGrounded
+    // Inheritance from BaseController
+    public class PlayerController : BaseController
     {
-        get => _isGrounded;
-        private set
+        // Serialized fields
+        [Space(10)]
+        [Header("Jump Settings")]
+        [SerializeField][Tooltip("Force applied to the player when jumping.")]
+        private float _jumpForce = 50f;
+        [SerializeField][Tooltip("Max jumps player can perform in air (does not include the first jump from the ground).")] 
+        private uint _maxInAirJumpCount = 1;
+    
+        // Private fields
+        private uint _inAirJumpCount;
+        private bool _isJumpPressed;
+    
+        // Constants
+        // Hashed string for animator parameters, this is to avoid using string directly to save performance
+        private static readonly int IsWalkingParam = Animator.StringToHash("isWalking");
+        private static readonly int IsGroundedParam = Animator.StringToHash("isGrounded");
+
+        protected override void Start()
         {
-            if (value != _isGrounded)
-            {
-                _isGrounded = value;
-                _isGroundedChanged = true;
-                if (value) _inAirJumpCount = 0;
-            }
+            base.Start();
+            
+            // Initialize in air jump count, in case player can jump in air at spawn
+            _inAirJumpCount = 0;
         }
-    }
-    
-    public bool IsWalking 
-    {
-        get => _isWalking;
-        private set
+
+        protected override void Update()
         {
-            if (value != _isWalking)
-            {
-                _isWalking = value;
-                _isWalkingChanged = true;
-            }
+            base.Update();
+            PlayerInput();
+            AnimationControl();
         }
-    }
 
-    public bool IsFlipped
-    {
-        get => _spriteRenderer.flipX;
-        private set
+        protected override void FixedUpdate()
         {
-            if (value != _spriteRenderer.flipX) _spriteRenderer.flipX = value;
+            base.FixedUpdate();
+            Jump();
         }
-    }
     
-    
-    // Serialized fields
-    [SerializeField] private float _moveSpeed = 50.0f;
-    [SerializeField] private float _acceleration = 2.0f;
-    [SerializeField] private float _friction = 2.0f;
-    [SerializeField] private float _jumpForce = 50f;
-    [SerializeField] private uint _maxInAirJumpCount = 1;
-    [SerializeField] private LayerMask _groundLayer;
-    [SerializeField] private string _groundTag = "Ground";
-    
-    // Components
-    private SpriteRenderer _spriteRenderer;
-    private CapsuleCollider2D _collider;
-    private Rigidbody2D _rigidbody;
-    private Animator _animator;
-    
-    // Private fields
-    private float _horizontalAxis;
-    private uint _inAirJumpCount;
-    private bool _isJumpPressed;
-    private float _lastPositionY;
-    private bool _isGrounded;
-    private bool _isGroundedChanged;
-    private bool _isWalking;
-    private bool _isWalkingChanged;
-    
-    // Constants
-    private static readonly int IsWalkingParam = Animator.StringToHash("isWalking");
-    private static readonly int IsGroundedParam = Animator.StringToHash("isGrounded");
-
-    private void Start()
-    {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _collider = GetComponent<CapsuleCollider2D>();
-        _rigidbody = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
-
-        _inAirJumpCount = 0;
-    }
-
-    private void Update()
-    {
-        PlayerInput();
-        AnimationControl();
-        FlipSprite();
-    }
-
-    private void FixedUpdate()
-    {
-        GroundCheck();
-        Jump();
-        Movement();
-    }
-    
-    private void PlayerInput()
-    {
-        _horizontalAxis = Input.GetAxis("Horizontal");
-        if (Input.GetButtonDown("Jump"))
+        private void PlayerInput()
         {
+            // Handle input and physics separately
+            _horizontalAxis = Input.GetAxis("Horizontal");
+        
+            if (!Input.GetButtonDown("Jump")) return;
             _isJumpPressed = true;
         }
-    }
 
-    private void Jump()
-    {
-        // Jump
-        bool canJump = (IsGrounded || _inAirJumpCount > 0) && _isJumpPressed;
-        _isJumpPressed = false;
+        private void Jump()
+        {
+            // Base on the input and other conditions, decide if the player can jump
+            bool canJump = (IsGrounded || _inAirJumpCount > 0) && _isJumpPressed;
+            _isJumpPressed = false;
+            
+            if (!canJump) return;
+            
+            _rigidbody.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+            
+            // if the player is grounded, reset in air jump count so player can jump again in the air
+            if (IsGrounded) ResetInAirJump();
+            // else, decrease in air jump count
+            else _inAirJumpCount--;
+        
+        }
 
-        if (!canJump) return;
-        
-        
-        _rigidbody.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
-        if (IsGrounded) ResetInAirJump();
-        else _inAirJumpCount--;
-        
-    }
-
-    private void ResetInAirJump()
-    {
-        _inAirJumpCount = _maxInAirJumpCount;
-    }
+        private void ResetInAirJump()
+        {
+            _inAirJumpCount = _maxInAirJumpCount;
+        }
     
-    private void Movement()
-    {
-        // Horizontal movements
-        var moveInputForce = new Vector2(_horizontalAxis * _acceleration * 1000.0f * Time.deltaTime, 0);
-        
-        if (IsGrounded) IsWalking = moveInputForce.x != 0;
-        else IsWalking = false;
-        
-        _rigidbody.AddForce(moveInputForce);
 
-        var currentVel = _rigidbody.velocity;
-        
-        var speedAbs = Mathf.Abs(currentVel.x);
-        if (speedAbs > _moveSpeed)
+        private void AnimationControl()
         {
-            _rigidbody.velocity = new Vector2(currentVel.x / speedAbs * _moveSpeed, currentVel.y);
+            // Update animator parameters only when bool values change
+            if (_isWalkingChanged)
+            {
+                _animator.SetBool(IsWalkingParam, IsWalking);
+                _isWalkingChanged = false;
+            }
+            if (_isGroundedChanged)
+            {
+                _animator.SetBool(IsGroundedParam, IsGrounded);
+                _isGroundedChanged = false;
+            }
         }
-        
-        if (moveInputForce.x != 0) return;
-        
-        // Add friction when player input stops
-        currentVel = _rigidbody.velocity;
-        speedAbs = Mathf.Abs(currentVel.x);
-        var frictionAmount = _friction * 1000.0f * Time.deltaTime;
-        
-        // Stop the player if the speed is too low
-        if (speedAbs < 0.1f || (speedAbs - frictionAmount < 0 && IsGrounded))
+
+        // Need some extra logic for ground check in player controller script
+        protected override void GroundCheck()
         {
-            _rigidbody.velocity = new Vector2(0, currentVel.y);
-            return;
+            base.GroundCheck();
+            
+            // If the player is grounded, reset in air jump count to 0 so when player fall from the edge of the ground,
+            // they won't be able to jump. Player can only perform air jump when jumping from the ground
+            if (IsGrounded) _inAirJumpCount = 0;
         }
-        
-        // Apply friction when grounded
-        if (!IsGrounded) return;
-        if (currentVel.x < 0) frictionAmount *= -1;
-        _rigidbody.AddForce(new Vector2(-frictionAmount, 0));
-    }
-
-    private void GroundCheck()
-    {
-        var position = transform.position;
-        
-        // Only check if the player has moved in the Y axis
-        if (Math.Abs(position.y - _lastPositionY) < 0.01f) return;
-        _lastPositionY = position.y;
-        
-        var origin = new Vector2(position.x, position.y - _collider.size.y / 2.0f);
-        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, 0.1f, _groundLayer);
-
-        IsGrounded = hit && hit.transform.CompareTag(_groundTag);
-    }
-
-    private void AnimationControl()
-    {
-        // Update animator parameters only when bool values change
-        if (_isWalkingChanged)
-        {
-            _animator.SetBool(IsWalkingParam, IsWalking);
-            _isWalkingChanged = false;
-        }
-        if (_isGroundedChanged)
-        {
-            _animator.SetBool(IsGroundedParam, IsGrounded);
-            _isGroundedChanged = false;
-        }
-    }
-
-    private void FlipSprite()
-    {
-        if (_horizontalAxis == 0) return;
-        IsFlipped = _horizontalAxis < 0;
     }
 }
