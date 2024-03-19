f243281fffbfef046a8dfadb4fbc3f34e48d83fa
johnloser <john-masterchief@hotmail.com>
2024-03-18 18:56:55 -0700

    create projectile and turret controller

---
+++ Assets/Scripts/Controller/ProjectileController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Health;
+using UnityEngine;
+
+namespace Controller
+{
+    [RequireComponent(typeof(SpriteRenderer), typeof(Collider2D))]
+    public class ProjectileController : MonoBehaviour
+    {
+        
+        
+        [SerializeField] private float _speed = 4.0f;
+        [SerializeField] private float _pushigForce = 12.0f;
+
+        private SpriteRenderer _spriteRenderer;
+        private BaseController _owner;
+
+        public void SetOwner(BaseController owner)
+        {
+            _owner = owner;
+        }
+
+        private void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            GetComponent<Collider2D>().isTrigger = true;
+        }
+
+        public void Flip()
+        {
+            _spriteRenderer.flipX = true;
+        }
+
+        private void Update()
+        {
+            // move
+            var dir = _spriteRenderer.flipX ? -1 : 1;
+            
+            transform.Translate(_speed * Time.deltaTime * dir, 0, 0);
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (!other.gameObject.CompareTag("Player"))
+                Destroy(gameObject);
+            
+            other.gameObject.TryGetComponent<CharacterHealth>(out CharacterHealth playerHealth);
+            if (!playerHealth) return;
+            playerHealth.TakeDamage(_owner);
+            var rg = other.rigidbody;
+            var dir = other.transform.position.x - transform.position.x;
+            rg.AddForce(new Vector2(dir > 0 ? 1:-1, 0) * _pushigForce, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/Controller/TurretController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+
+namespace Controller
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class TurretController : MonoBehaviour
+    {
+        [SerializeField] private ProjectileController _projectile;
+        [SerializeField] private float _shootingInterval = 2.0f;
+        [SerializeField] private GameObject _projectileSpawnPoint;
+        
+        
+        private Transform _playerTransform;
+        private SpriteRenderer _spriteRenderer;
+
+        private float _shootTimer;
+        
+        private void Start()
+        {
+            var player = GameObject.FindWithTag("Player");
+            if (player) _playerTransform = player.transform;
+
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+
+            ResetShootTimer();
+        }
+
+        private void ResetShootTimer()
+        {
+            _shootTimer = _shootingInterval;
+        }
+
+        private void Update()
+        {
+            Flip();
+            Shooting();
+        }
+
+        private void Shooting()
+        {
+            _shootTimer -= Time.deltaTime;
+
+            if (_shootTimer > 0) return;
+
+            if (_projectile)
+            {
+                if (_spriteRenderer.flipX) _projectile.Flip();
+                Instantiate(_projectile, _projectileSpawnPoint.transform.position, Quaternion.identity);
+            }
+            
+            ResetShootTimer();
+        }
+
+        private void Flip()
+        {
+            var playerX = _playerTransform.position.x;
+            var x = transform.position.x;
+
+            if (x > playerX)
+            {
+                _spriteRenderer.flipX = true;
+            }
+            else if (x < playerX)
+            {
+                _spriteRenderer.flipX = false;
+            }
+        }
+    }
+}
